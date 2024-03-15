ac1f97e5ce34e386ce21aeae74476163159d209d
sahil <user@users-MacBook-Pro.local>
2024-03-15 19:47:23 +0530

    done vercel settings version 1

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo " BUILD START"
-python3.12 -m pip install --upgrade pip
-python3.12 -m pip install -r requirements.txt
-python3.12 manage.py collectstatic --noinput --clear
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+python manage.py collectstatic --noinput --clear
 echo "BUILD END"
